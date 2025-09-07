0e93f54ca3fea5199f67f8911c230fb20bca591a
Josh <josh.vansantvoord@maine.edu>
2025-09-07 14:12:09 -0400

    Added Hello World text

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App(): React.JSX.Element {
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. Josh VanSantvoord
+                automatically reload. Josh VanSantvoord Hello World
             </p>
         </div>
     );
